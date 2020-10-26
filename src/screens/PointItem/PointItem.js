d647ce0c89797a78a8f3c867ec1ab431d1be0e9a
Manuel Romero <manu8083el@gmail.com>
2020-10-26 00:18:55 -0500

    creacion de nuevo view

---
+++ src/screens/PointItem/PointItem.js
@@ -0,0 +1,15 @@
+import React, { Component, useEffect, useState } from 'react'
+import { View , Image, FlatList} from 'react-native';
+
+
+
+export default function PointItem() {
+
+
+  return (
+    <View>
+        <Text>HOLA MUNDO</Text>
+  </View>
+  );
+
+}
